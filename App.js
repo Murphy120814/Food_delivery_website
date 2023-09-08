8614096457d04e1a79a1f0bd1bd2f7457e7e04ca
Murphy <109974838+Murphy120814@users.noreply.github.com>
2023-09-08 17:00:58 +0530

    initial JSX
    Introduction to JSX

--- App.js
+++ App.js
@@ -2,36 +2,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const headingChildren1 = React.createElement(
-  "h1",
-  { id: "heading1", key: "heading1" },
-  "Hello I am heading1"
-);
-const headingChildren2 = React.createElement(
-  "h1",
-  { id: "heading2", key: "heading2" },
-  "Hello I am heading2"
-);
-const headingChildren3 = React.createElement(
-  "h1",
-  { id: "heading3", key: "heading3" },
-  "Hello I am heading3"
-);
-const headingChildren4 = React.createElement(
-  "h1",
-  { id: "heading4", key: "heading4" },
-  "Hello I am heading4"
-);
+const Heading = (
+  <h1 id="heading1" className="heading">
+    Hello, THis is heading using JSX
+  </h1>
+); //this is called as jsx element
 
-const parentDiv = React.createElement("div", { id: "parent" }, [
-  headingChildren1,
-  headingChildren2,
-  headingChildren3,
-  headingChildren4,
-]);
-
-console.log(parentDiv);
-//parent can take array of children element if there are multiple children elements
+//Babel will convert jsx element into createELement object Babel will be managed by the parcel
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parentDiv);
+root.render(Heading);
