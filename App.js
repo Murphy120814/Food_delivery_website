6ffde2395b7efec0249e4014e6215c072d02c87e
Murphy <109974838+Murphy120814@users.noreply.github.com>
2023-09-08 17:55:21 +0530

    Playing around with functional component
    usage of functional component and how to use them and how to create them + creating and rendering react element as well as functional component

--- App.js
+++ App.js
@@ -2,13 +2,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Heading = (
-  <h1 id="heading1" className="heading">
-    Hello, THis is heading using JSX
-  </h1>
-); //this is called as jsx element
+const AnotherFunctionalComponent = () => (
+  <h1 id="heading3"> THis is called as component composition!!! </h1>
+);
+
+const Heading2 = () => (
+  <h1>THis is JSX element returning using implicit return</h1>
+);
+//functional component using Explicit return
+function Heading() {
+  return (
+    <>
+      <h1 className="heading" id="heading1">
+        Hello, this is functional component
+      </h1>
+      <Heading2 />
+      {AnotherFunctionalComponent()}
+    </>
+  );
+}
+
+//functional component using explicit return
 
 //Babel will convert jsx element into createELement object Babel will be managed by the parcel
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(Heading);
+root.render(<Heading />);
